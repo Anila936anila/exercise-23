dca54d5976cce141dbaa7389cf08ce9ba5f5b396
Anila936anila <161087832+Anila936anila@users.noreply.github.com>
2024-02-27 12:52:19 -0800

    Conditional Test
    Typescript 45Q assignment exercise 23

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+let car = 'subaru';
+console.log("is car == 'subaru'? I predict True.");
+console.log(car == 'subaru'); //True
+console.log("Is car == 'honda'? I predict False.");
+console.log(car == 'honda'); //False
+console.log("is car === 'subaru'? I predict True.");
+console.log(car === 'subaru'); //True
+console.log("Is car === 'Subru'? I predict False.");
+console.log(car === 'Subru'); //False
+console.log("is car !== 'toyota'? I predict True.");
+console.log(car !== 'toyota'); //True
+console.log("is car !== 'subru'? I predict True.");
+console.log(car !== 'subru'); //False
+console.log("is car !== 'mazda'? I predict True.");
+console.log(car !== 'mazda'); //True
+console.log("is car !== 'subru'? I predict True.");
+console.log(car !== 'subru'); //False
+let age = 25;
+console.log("Is age > 18? I predict True.");
+console.log(age > 18); // True
+console.log("Is age < 21? I predict False.");
+console.log(age < 21); // False
